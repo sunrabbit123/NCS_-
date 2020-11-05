eda2386c5fd28f3fa16a340b0448ecc88c44ce9a
sunrabbit123 <qudwls185@naver.com>
2020-11-05 21:01:19 +0900

    add routing

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const routes = require('./router/index');
 
-const routes = require('../router/index');
-const http = require('http');
+const app = express();
 
-app = express();
-const server = http.createServer(app)
+const port = process.env.PORT || 4000;
+const { jwtMiddleware } = require('./lib/token');
 
-const port = 5000;
+//node 네이티브 promise 사용
+mongoose.Promise = global.Promise;
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        autoIndex: true,
+    })
+    .then(() => {
+        console.log('Successfully connected to mongodb');
 
-app.use(express.json())
-    .use(express.urlencoded({ extended: true }))
-    .set('views', __dirname + '/views')
-    .set('view engine', 'html')
-    .use(express.static('public'))
-    .listen(port, () => {
-        console.log('it is listening to port 5252');
-        console.log(`open http://localhost:${port}`);
-    }
-);
+        //express 기본 세팅
+        app.use(express.json());
+        app.use(cookieParser());
+        app.use(jwtMiddleware);
+        app.use(express.urlencoded({ extended: true }));
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'html');
+        app.engine('html', require('html').renderFile);
+
+        //정적파일들의 경로를 public으로 향하게
+        app.use(express.static('public'));
+
+        // 로그 남기기
+        app.use(morgan('common'));
+
+        //라우팅
+        app.use('/', routes);
+        
+        //포트 열고 링크 남기기
+        app.listen(port, () => {
+            console.log('it is listening to port 5252');
+            console.log(`open http://localhost:${port}`);
+        })
+    })
+    .catch(console.error);
---
+++ router/border/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+// 게시판 보기
+router.get('/', async(_, res) =>{
+    
+});
+
+// 게시판 생성창
+router.get('/add', async(_, res) => {
+
+});
+
+// 게시판 읽기 창
+router.get('/read', async(req, res)=> {
+
+});
+
+module.exports = router;

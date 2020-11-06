4a774b598f17b525ecb1aab2821f7c7b57b5274c
sunrabbit123 <qudwls185@naver.com>
2020-11-06 14:02:38 +0900

    add footer css

--- router/index.js
+++ router/index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 router.use('/border', require('./border'));
 
-router.get('/',async (_, res) => {
+router.get('/', async (_, res) => {
     res.render('index');
 });
 
+router.get('/copyright', async(_, res) => {
+    res.render('LICENSE');
+});
 module.exports = router;

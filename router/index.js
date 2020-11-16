0e52aee7e7e0423fd27fda28cbcb5b2ecb91b571
sunrabbit123 <qudwls185@naver.com>
2020-11-16 11:47:51 +0900

    add render posts

--- router/index.js
+++ router/index.js
@@ -4,8 +4,45 @@ const router = express.Router();
 
 router.use('/border', require('./border'));
 
-router.get('/', async (_, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+    posts = {
+        posts: [{
+            title: '대충 제목',
+            publisher: '대충 작성자',
+            created_at : '대충 작성일',
+            description : '대충 내용'
+        },
+        {
+            title: '대충 제목',
+            publisher: '대충 작성자',
+            created_at : '대충 작성일',
+            description : '대충 내용'
+        },
+        {
+            title: '대충 제목',
+            publisher: '대충 작성자',
+            created_at : '대충 작성일',
+            description : '대충 내용'
+        },
+        {
+            title: '대충 제목',
+            publisher: '대충 작성자',
+            created_at : '대충 작성일',
+            description : '대충 내용'
+        },
+        {
+            title: '대충 제목',
+            publisher: '대충 작성자',
+            created_at : '대충 작성일',
+            description : '대충 내용'
+        },
+
+    ],
+    page : parseInt(req.query.page || 1),
+    postCnt : 1
+    };
+    
+    res.render('index', posts);
 });
 
 router.get('/copyright', async(_, res) => {
